cf4c13ca562849f2fc691f1d47b37ed0851ca221
peter chang <changcassandra02@gmail.com>
2024-12-01 20:13:23 +0700

    building test 3

--- unit_test.py
+++ unit_test.py
@@ -20,27 +20,5 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Sugar Cane Yield Prediction', response.data)  # Check if the content is as expected
 
-    # def test_prediction(self):
-    #     # Test the prediction route with valid mock data
-    #     raw_mock_data = {
-    #         'wind_speed': 5.0,
-    #         'temp': 30.0,
-    #         'hum': 60.0,
-    #         'heat_idx': 32.0,
-    #         'pres': 1013.0,
-    #         'prec': 50.0,
-    #         'fossil_co2_em': 800.0,
-    #         'co2_em_change': 1.2,
-    #         'co2_em_per_capita': 4.5,
-    #         'population': 1000000,
-    #         'pop_change': 2.1
-    #     }
-    #     raw_mock_data = pd.DataFrame([raw_mock_data])
-    #     print(raw_mock_data)
-    #     # Send the scaled data to the /result endpoint
-    #     response = self.app.post('/result', data=raw_mock_data)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertIn(b'Next Year Crop Yield:', response.data)  # Ensure the result page contains prediction text
-
 if __name__ == '__main__':
     unittest.main()
